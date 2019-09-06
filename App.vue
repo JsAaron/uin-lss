9becce45c790bc4c8e0a789e84fb72215d5d103b
jsaaron <401545553@qq.com>
2019-09-06 11:20:52 +0800

    test

--- App.vue
+++ App.vue
@@ -13,5 +13,32 @@
 </script>
 
 <style>
-	/*每个页面公共css */
+	page {
+		width: 100%;
+		height: 100%;
+		box-sizing: border-box;
+		font-size: 16px;
+		-webkit-font-smoothing: antialiased;
+		// font-family: "PingFang SC", Helvetica, "STHeiti STXihei", "Microsoft YaHei",
+		//   Tohoma, Arial, sans-serif;
+		font-family: Arial;
+		background: rgb(242, 242, 242);
+		position: relative;
+		font-size: 16px;
+		font-family: -apple-system-font, Helvetica Neue, Helvetica, sans-serif;
+		-ms-text-size-adjust: 100%;
+		-webkit-text-size-adjust: 100%;
+		-webkit-tap-highlight-color: transparent;
+	}
+
+	/* 默认去边框  */
+	button[plain] {
+		border: 0;
+	}
+
+	// 去边框
+	button::after {
+		border: none;
+	}
+
 </style>
