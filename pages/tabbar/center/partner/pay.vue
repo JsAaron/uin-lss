e2e1c0e80a2a83f6fbfd5fad4df126436a408371
wen <>
2019-09-13 21:25:15 +0800

    123

--- pages/tabbar/center/partner/pay.vue
+++ pages/tabbar/center/partner/pay.vue
@@ -1,20 +1,64 @@
 <template>
-	<view></view>
+	<view>
+		<lss-payment :dataSet="dataSet">
+			<view class="header">
+				<view class="header__row">
+					<label class="header__label">昵称</label>
+					<text class="header__text">{{ title }}</text>
+				</view>
+				<view class="header__row">
+					<label class="header__label">付款金额</label>
+					<text>￥{{ money }}</text>
+				</view>
+				<view class="header__row">
+					<label class="header__label">手机号</label>
+					<text>{{ telephone }}</text>
+				</view>
+				<view class="header__row">
+					<label class="header__label">收货地址</label>
+					<text class="header__text">{{ address }}</text>
+				</view>
+			</view>
+		</lss-payment>
+	</view>
 </template>
 
 <script>
 import * as util from '@/utils';
 import { $$set, $$get } from '@/common/global';
+import lssPayment from '@/pages/common/payment';
 export default {
-	components: {},
+	components: {
+		lssPayment
+	},
 	data() {
-		return {};
+		return {
+			title: '',
+			money: '',
+			telephone: '',
+			address: '',
+			dataSet: {
+				order_id: '',
+				money: '',
+				is_royalty: '',
+				is_fx: '',
+				agentid: ''
+			}
+		};
 	},
 	props: {},
 	created() {},
 	onLoad(options) {
-		console.log(options)
-		options.data = `{"order_id":"2019091317562380172","amount":"399","consignee":"陈文","telephone":"13411111111","address":"是打发送达方式123是打发是"}`
+		options.data = `{"order_id":"2019091317562380172","amount":"0.01","consignee":"陈文","telephone":"13411111111","address":"是打发送达方式123是打发是"}`;
+		let data = JSON.parse(options.data);
+		this.dataSet = {
+			order_id: data.order_id,
+			title: data.consignee,
+			money: data.amount,
+			telephone: data.telephone,
+			address: data.address,
+			agentid: '7080007'
+		};
 	},
 	methods: {}
 };
